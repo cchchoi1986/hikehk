c286e6bca0b3a59e6f7dab67fdaae030200e3c82
cchchoi1986 <cchchoi1986@gmail.com>
2015-02-26 17:05:10 +0800

    rails controller and search jbuilder created

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -10,6 +10,36 @@ class TrailsController < ApplicationController
   end
 
   def search
+    duration = params[:duration].to_i
+    distance = params[:distance].to_i
+    scenery = params[:scenery].to_i
+    difficulty = params[:difficulty].to_i
+    hk = params[:hk]
+    kln = params[:kln]
+    nt = params[:nt]
+    regions = []
 
+
+    if hk == "true"
+      # regions.push(".where(region_id: 15..18)")
+      regions.push((15..18).to_a)
+    end
+    if kln == "true"
+      # regions.push(".where(region_id: 10..14)")
+      regions.push((10..14).to_a)
+    end
+    if nt == "true"
+      # regions.push(".where(region_id: 1..9)")
+      regions.push((1..9).to_a)
+    end
+    regions.flatten!
+
+    puts ">>>>>>>>>>>>> #{regions}"
+
+    query = Trail.where(duration: 0..duration).where(distance: 0..distance).where(difficulty: 0..difficulty).where(scenery: 0..scenery).where(region_id: regions)
+    @query = query.order("name")
+
+    render 'search.json.jbuilder'
   end
+
 end
