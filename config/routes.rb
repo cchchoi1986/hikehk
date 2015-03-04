4bc1c0122e5e1bfd93e82b30f2d22a35f46763b5
cchchoi1986 <cchchoi1986@gmail.com>
2015-03-04 15:51:23 +0800

    added search by id

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   get '/trails/:id', to: 'trails#show'
   get '/trails', to: 'trails#index'
   get '/search', to: 'trails#search'
-  get '/trails/:id/flora', to: 'plants#show'
-  get '/flora', to: 'plants#index'
-  get '/trails/:id/fauna', to: 'birds#show'
-  get '/fauna', to: 'birds#index'
+  get '/searchid', to: 'trails#searchid'
+  # get '/trails/:id/flora', to: 'plants#show'
+  # get '/flora', to: 'plants#index'
+  # get '/trails/:id/fauna', to: 'birds#show'
+  # get '/fauna', to: 'birds#index'
 end
